a2ca0202f6b6dc7a98fcaf257fb6c2c732778fcb
Cookieemonsterr <naheeeelb03@gmail.com>
2025-11-05 20:06:37 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    strictPort: true,
-  },
-});
+  // IMPORTANT: repo name here
+  base: '/qc-buddy-frontend/',
+})
